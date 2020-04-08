f008971c7fb88b7eb0dd9d4466640115922b0004
Alessio Pau <a.pau@avanade.com>
2020-04-08 18:19:37 +0200

    Aggiunto metodo upload file

---
+++ CallAAD_APP/SharepointOperations.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+using File = System.IO.File;
+using SharepointFile = Microsoft.SharePoint.Client.File;
+
+namespace CallAAD_APP
+{
+    public static class SharepointOperations
+    {
+
+
+        private static void UploadFileOnSharepoint(ClientContext context)
+        {
+            string filePath = ConfigurationSettings.AppSettings["ClientSecret"];
+
+            using (FileStream fileStream = File.OpenRead(filePath))
+            {
+                String fileName = System.IO.Path.GetFileName(filePath);
+                Web web = context.Web;
+
+                context.Load(web.Lists);
+                context.ExecuteQuery();
+
+                List library = web.Lists.GetByTitle("DocumentiAlessio");
+
+                context.Load(library.RootFolder);
+                context.Load(library.RootFolder.Files);
+                context.ExecuteQuery();
+
+                FileCreationInformation uploadFile = new FileCreationInformation();
+                uploadFile.ContentStream = fileStream;
+                uploadFile.Url = fileName;
+                uploadFile.Overwrite = true;
+
+
+                SharepointFile spfile = library.RootFolder.Files.Add(uploadFile);
+
+                library.RootFolder.Update();
+                context.ExecuteQuery();
+            }
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
